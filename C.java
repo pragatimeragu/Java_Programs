0b9496681a88eed8d69f044e8d71625027d09936
pragatimeragu <pragatimeragu3657@gmail.com>
2026-05-18 20:59:43 +0530

    Upcasting code added

--- C.java
+++ C.java
@@ -36,4 +36,8 @@ class C extends B
 //in this process B b1 = new C();	- both Compiler and JVM are working
 // 1 st Compiler starts from reference i,e class B if found the method then says to JVM 
 // But JVM again verifies and says that b1 is object of class C and object has more importance 
-//then it executes the method from Class C becoz b1 is object of class C 
+//then it executes the method from Class C becoz b1 is object of class C
+// here if there is no such method show in parent (B class ) then ouput is Error / Exception
+// the method always present in parent class 
+// and another case if method present in parent class as well as child then the method excuted whoevers class object is created
+// next case if method is not present in object class (i,e in this case C) then method executed from refernce class (B) 
