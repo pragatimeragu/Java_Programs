2f61e15bcdab97452321564e243768af806bbcdd
pragatimeragu <pragatimeragu3657@gmail.com>
2026-07-12 20:56:23 +0530

    Pattern2 code added

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,22 @@
+//          *
+//          **
+//          ***
+//          ****
+//          *****
+
+class Pattern2 {
+    static public void pattern2(int n) {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+     
+    }
+    public static void main(String[] args) {
+        pattern2(6);
+    }
+}
