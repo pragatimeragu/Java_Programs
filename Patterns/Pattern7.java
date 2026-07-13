429b85cf21063913b53a2704da108783f9802021
pragatimeragu <pragatimeragu3657@gmail.com>
2026-07-13 21:11:48 +0530

    Pattern8 added

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,37 @@
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+class Pattern7 {
+    static public void pattern7(int n) {
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+             System.out.print(" ");
+
+            }
+         
+
+            for(int j=1;j<=2*i-1;j++)
+            {
+              
+                
+                    System.out.print("*");
+                
+                
+            }
+            System.out.println();
+
+           
+        }
+       
+     
+    }
+    public static void main(String[] args) {
+        pattern7(5);
+    }
+}
