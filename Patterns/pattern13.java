a503abdb6d398ab3b71ca20c33681b33b198aecd
pragatimeragu <pragatimeragu3657@gmail.com>
2026-07-16 19:30:29 +0530

    Pattern13 code added

---
+++ Patterns/pattern13.java
@@ -0,0 +1,23 @@
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15
+
+class pattern13 {
+    static public void pattern13(int n) {
+        int count=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern13( 5);
+    }
+}
