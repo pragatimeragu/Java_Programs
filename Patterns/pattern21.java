f572d733ce6d89943dee039130101c69462af79e
pragatimeragu <pragatimeragu3657@gmail.com>
2026-07-17 11:20:29 +0530

    pattern 21 added

---
+++ Patterns/pattern21.java
@@ -0,0 +1,36 @@
+/*
+
+*****
+*   *
+*   *
+*   *
+*****
+
+*/
+
+class pattern21 {
+     static public void pattern21(int n) {
+     
+         for(int i=1;i<=n;i++)
+        {
+           
+            for(int j=1;j<=n;j++)
+            {
+                if(i==1||j==1||i==n||j==n)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+               
+            }
+           
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern21(5);
+    }
+}
