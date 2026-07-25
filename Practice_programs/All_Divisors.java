da9e1496fd3cc1d19d5c8751fdc6225e87d82afc
pragatimeragu <pragatimeragu3657@gmail.com>
2026-07-25 13:17:17 +0530

    AllDivisors code added

--- Practice_programs/All_Divisors.java
+++ Practice_programs/All_Divisors.java
@@ -1,14 +1,32 @@
+import java.util.Arrays;
+
 class All_Divisors {
     static public int[] divisors(int n) {
+        int count =0;
         for(int i=1;i<=n;i++)
         {
             if(n%i==0)
             {
-                return new int[] {i};
+                count++;
             }
         }
+
+        int[] divisors = new int[count];
+        int index=0;
+
+         for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                divisors[index]=i;
+                index++;
+            }
+        }
+        return divisors;
+
     }
     public static void main(String[] args) {
-        divisors(8);
+        System.out.println(Arrays.toString(divisors(8)));
+        System.out.println(Arrays.toString(divisors(18)));
     }
 }
