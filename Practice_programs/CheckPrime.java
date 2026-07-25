c1554c83354b60b92d10a7cdab9d95f5e1dbf0a0
pragatimeragu <pragatimeragu3657@gmail.com>
2026-07-25 14:23:43 +0530

    Prime no code added

---
+++ Practice_programs/CheckPrime.java
@@ -0,0 +1,20 @@
+public class CheckPrime {
+    static public boolean isPrime(int n) {
+        if(n<=1)
+        {
+            return false;
+        }
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+               return false;
+            }
+        }
+        return true;
+        
+    }
+    public static void main(String[] args) {
+        System.out.print(isPrime(12));
+    }
+}
